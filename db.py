62fa613213dc647d54a5b5adfa0ce5763c46e26f
Luke Oess <lukeoess@gmail.com>
2025-09-13 11:59:17 -0500

    separate functions from main

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+DB_FILE = "spinlog.db"
+
+def get_connection():
+    conn = sqlite3.connect(DB_FILE)
+    conn.execute("PRAGMA foreign_keys = ON;") # foreign key enforcement
+    cursor = conn.cursor()
+
+
+# fix the rest of these so they use get connection() function bc now they dont work 
+
+def insert_artist(name):
+    try: 
+        cursor.execute("INSERT INTO Artist (name) VALUES(?)", (name,))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print(f"Artist '{name}' already exists")
+        
+#def insert_release(release_name, artist_name, year, format): 
+    
+def print_all_artists():
+    cursor.execute("""
+               SELECT *
+               FROM Artist               
+               """)
+    print(cursor.fetchall())
+    
+   
+
+
+
+cursor.execute("DELETE FROM Artist WHERE id = ?", (4,))
+conn.commit()
+
+print_all_artists()
