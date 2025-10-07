fe7bccdd9355974588f8cc402b5693e77c302b39
Luke Oess <lukeoess@gmail.com>
2025-10-07 15:04:21 -0500

    added correct connection argument to method parameters

--- seed.py
+++ seed.py
@@ -2,20 +2,31 @@ import sqlite3
 
 import db
 
-# small seed to recreate test data quickly
-db.insert_artist("Frank Sinatra")
-db.insert_album("In the Wee Small Hours", "Frank Sinatra", 1955, "LP")
+
+def seed_data(): 
+    try: 
+        with db.get_connection() as conn: 
+            print("seeding database")
+            db.insert_artist(conn, "Frank Sinatra")
+            db.insert_album(conn, "In the Wee Small Hours", "Frank Sinatra", 1955, "LP")
 
 
-db.insert_artist("Mac Demarco")
-db.insert_album("Guitar", "Mac Demarco", 2025, "LP")
-db.insert_album("Another One", "Mac Demarco", 2015, "LP")
+            db.insert_artist(conn, "Mac Demarco")
+            db.insert_album(conn, "Guitar", "Mac Demarco", 2025, "LP")
+            db.insert_album(conn, "Another One", "Mac Demarco", 2015, "LP")
 
-db.insert_artist("Elijah Fox")
-db.insert_album("Wyoming", "Elijah Fox", 2023, "LP")
+            db.insert_artist(conn, "Elijah Fox")
+            db.insert_album(conn, "Wyoming", "Elijah Fox", 2023, "LP")
 
-db.insert_artist("Laufey")
-db.insert_album("Bewitched", "Laufey", 2022, "LP")
+            db.insert_artist(conn, "Laufey")
+            db.insert_album(conn, "Bewitched", "Laufey", 2022, "LP")
 
-db.insert_artist("Pink Floyd")
-db.insert_album("The Dark Side of the Moon", "Pink Floyd", 1975, "LP")
+            db.insert_artist(conn, "Pink Floyd")
+            db.insert_album(conn, "The Dark Side of the Moon", "Pink Floyd", 1975, "LP")
+            print("seed complete")
+    except (sqlite3.IntegrityError, ValueError) as e: 
+        print(f"An error occurred while seeding: {e}")
+
+if __name__ == "__main__":
+    seed_data()
+
